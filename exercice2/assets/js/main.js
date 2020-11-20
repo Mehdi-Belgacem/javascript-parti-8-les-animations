e0e7f80e68f5d758b5da8d28cfb556b2ff44b933
Mehdi-Belgacem <72203119+Mehdi-Belgacem@users.noreply.github.com>
2020-11-20 13:52:21 +0100

    envoi de la parti 8

---
+++ exercice2/assets/js/main.js
@@ -0,0 +1,19 @@
+let firstFable = document.getElementById('fable1');
+let btnFirstFable = document.getElementById('btnFirstFable');
+btnFirstFable.addEventListener('click', function () { firstFable.style.fontWeight = 'bold'; firstFable.style.color = 'red'; firstFable.style.fontSize = '2rem'; })
+
+let secondFable = document.getElementById('fable2');
+let btnSecondFable = document.getElementById('btnSecondFable');
+btnSecondFable.addEventListener('click', function () { secondFable.style.fontWeight = 'bold'; secondFable.style.color = 'purple'; secondFable.style.fontSize = '2rem'; })
+
+let thirdFable = document.getElementById('fable3');
+let btnThirdFable = document.getElementById('btnThirdFable');
+btnThirdFable.addEventListener('click', function () { thirdFable.style.fontWeight = 'bold'; thirdFable.style.color = 'green'; thirdFable.style.fontSize = '2rem'; })
+
+let fourthFable = document.getElementById('fable4');
+let btnFourthFable = document.getElementById('btnFourthFable');
+btnFourthFable.addEventListener('click', function () { fourthFable.style.fontWeight = 'bold'; fourthFable.style.color = 'orange'; fourthFable.style.fontSize = '2rem'; })
+
+let fifthFable = document.getElementById('fable5');
+let btnFifthFable = document.getElementById('btnFifthFable');
+btnFifthFable.addEventListener('click', function () { fifthFable.style.fontWeight = 'bold'; fifthFable.style.color = 'blue'; fifthFable.style.fontSize = '2rem'; fifthFable.style.background = 'black'; })
---
+++ exercice3/assets/js/main.js
@@ -0,0 +1,18 @@
+let paragraphs = document.getElementsByTagName('p');
+
+window.addEventListener('scroll', showParagrapheOnScroll); // Évènement sur le scroll de la page \\
+
+function showParagrapheOnScroll() {
+    for (let p of paragraphs) {
+        let positionElementTop = p.offsetTop; // récupérer la différence du haut de la page et du haut du paragraphe
+        let positionScrollY = window.scrollY; // récupérer la position de la scroll bar
+        let windowHeight = window.innerHeight - 200; // récupere la hauteur de la fênètre intérieur en retirant 200px
+        let result = positionScrollY + windowHeight; // postion de la scroll bar + la hauteur de la fênètre intérieur avec les -200px
+        if (result > positionElementTop) { // condition si le résultat est supérieur à la position haute de l'élément
+            p.classList.add('pVisible');
+        } else {
+            p.classList.remove('pVisible');
+        }
+    }
+}
+showParagrapheOnScroll() // executer la fonction après le chargement de la page pour afficher les paragraphe qui peuvent être afficher
