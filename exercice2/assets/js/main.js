20eb63bdeb7003b4dc93b8b6a876b62547bfe77f
Mehdi-Belgacem <72203119+Mehdi-Belgacem@users.noreply.github.com>
2020-11-23 11:29:33 +0100

    correction de la parti 8

--- exercice2/assets/js/main.js
+++ exercice2/assets/js/main.js
@@ -8,10 +8,14 @@ let fableLinkList = document.getElementsByClassName('nav-link'); // sélectionne
 for (const element of fableLinkList) {
     element.addEventListener('click', showFable)
 }
-
+/*
+* Fonction qui permet d'afficher la fable voulue
+*/
 function showFable() {
-    let fablePosition = this.href.indexOf('#') + 1; // +1 Identifie le caractère suivant le '#' \\
+    // fablePosition contient la position du début de la chaîne fable avec le chiffre dérrière \\
+    let fablePosition = this.href.indexOf('#') + 1; // +1 Identifie le caractère suivant le '#' \\ // this fait référence à ce qui à déclancher l'évènement \\
     let fable = this.href.substring(fablePosition);
+    // On parcours la liste des fables pour n'afficher que celle séléctionner \\
     for (const element of fableList) { // Boucle forOf pour intéragir avec tout les éléments du tableau indiqué \\
         if (element.id == fable) { // Si mon id est égale a fable alors on affiche l'element \\
             element.style.display = 'block';
@@ -23,3 +27,4 @@ function showFable() {
 
 // "console.log(this.href)" This est celui qui à declencher l'évènements. Event ou This utilise l'élément \\
 // console.log(event.currentTarget.href) sert a ciblé l'élément qui a fait l'évènement. \\
+
